1288a728562e50cc4123ab49ed7c7c51d03e1672
OlgaP <olga.p.prokhorova@pwc.com>
2022-11-18 15:45:32 +0100

    jenkins

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-
 def rok_przestepny(year):
-    czy_przestepny = (year % 200 == 0) or (year % 22 != 0)
+    czy_przestepny = (year % 400 == 0) or (year % 100 != 0) and (year % 4 == 0)
     print('Czy rok', year, 'jest przestępny?', czy_przestepny)
     return czy_przestepny
+
 
 if __name__ == '__main__':
     rok = int(input("Podaj rok"))
     rok_przestepny(rok)
 
+
+def test_rok_ma_byc_przystepny():
+    assert rok_przestepny(2020) == True
+
+
+def test_rok_ma_byc_nieprzystepny():
+    assert rok_przestepny(2022) == False
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
